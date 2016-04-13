d33a42fca35a1197ecf2c7c76cfc426c3aa444fd
shrimpson <andrewjpaterson88@gmail.com>
2016-04-12 22:03:49 -0700

    Updated Name

--- app/src/main/java/com/avalanche2/a2passwordgenerator/Generator.java
+++ app/src/main/java/com/avalanche2/a2passwordgenerator/Generator.java
@@ -5,14 +5,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -35,13 +30,6 @@ public class Generator extends AppCompatActivity {
     private int numberOfDigits = 1;
 
 
-
-    /**
-     * Sets the number picker's text to white.
-     * @param numberPicker
-     * @param color
-     * @return
-     */
     public static boolean setNumberPickerTextColor(NumberPicker numberPicker, int color)
     {
         final int count = numberPicker.getChildCount();
@@ -63,8 +51,8 @@ public class Generator extends AppCompatActivity {
                 catch(IllegalAccessException e){
                     Log.w("setNumberPickerTextColr", e);
                 }
-                catch(IllegalArgumentException e){
-                    Log.w("setNumberPickerTextColo", e);
+                catch(IllegalArgumentException f){
+                    Log.w("setNumberPickerTextColo", f);
                 }
             }
         }
@@ -82,11 +70,11 @@ public class Generator extends AppCompatActivity {
                     pf.set(picker, colorDrawable);
                 } catch (IllegalArgumentException e) {
                     e.printStackTrace();
-                } catch (Resources.NotFoundException e) {
-                    e.printStackTrace();
+                } catch (Resources.NotFoundException g) {
+                    g.printStackTrace();
                 }
-                catch (IllegalAccessException e) {
-                    e.printStackTrace();
+                catch (IllegalAccessException h) {
+                    h.printStackTrace();
                 }
                 break;
             }
